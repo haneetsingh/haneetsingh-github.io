a1c628bfba6bd43beae9a3888128b2b7f1ce6149
Haneet Singh <singh.haneet007@gmail.com>
2021-02-15 23:46:51 +0100

    Portfolio site

---
+++ gatsby/config.js
@@ -0,0 +1,290 @@
+module.exports = {
+  siteUrl: 'https://haneetsingh.github.io',
+  siteTitle: 'Haneet Singh - Web Developer',
+  manifestName: 'Portfolio - Haneet Singh',
+  manifestShortName: 'Portfolio - Haneet Singh',
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#ced4da',
+  manifestThemeColor: '#ced4da',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`,
+  firstName: 'Haneet',
+  lastName: 'Singh',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/haneetsingh',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/haneetsingh/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/iSinghHaneet',
+    },
+    {
+      icon: 'fa-drupal',
+      name: 'Drupal',
+      url: 'https://www.drupal.org/u/singh_haneet',
+    }
+  ],
+  email: 'singh.haneet007@gmail.com',
+  address: 'Amsterdam, Netherlands',
+  about: [
+    'I am an open-source enthusiast and passionate web developer. I like to build incredible visual interfaces, user experiences, branding assets and guidelines. I am experienced in modern Javascript frameworks like React.js, Vue.js, Angular, Drupal frontend & backend, PHP, Node.js, Express, MongoDB, PostgreSQL, MySQL, Sass/LESS and HTML/CSS.'
+  ],
+  experience: [
+    {
+      position: 'Senior Consultant',
+      organisation: 'ING Bank',
+      from: 'Oct 2020',
+      to: 'Present',
+      projects: [
+        {
+          title: 'ING Web Apps - Netherlands and Belgium',
+          description: 'Responsible for developing the web banking applications using the latest web standards and technologies like ES Modules, Web Components, LitElement and lit-html',
+          // stack: 'Node.js, Express, React.js, PostgreSQL, MongoDB'
+        }
+      ]
+    },
+    {
+      position: 'Senior Technical Lead',
+      organisation: 'FINNEO',
+      from: 'June 2020',
+      to: 'Oct 2020',
+      projects: [
+        {
+          title: 'NEO',
+          description: 'Leading the in-house software development team for development of a Commercial Real Estate debt financing platform (NEO) that automatically matches borrowers and lenders using real time data points.',
+          stack: 'Node.js, Express, React.js, PostgreSQL, MongoDB'
+        }
+      ]
+    },
+    {
+      position: 'Freelance Consultant',
+      organisation: 'Freelance',
+      from: 'September 2018',
+      to: 'May 2020',
+      projects: [
+        {
+          title: 'SmartKPIs',
+          description: 'Lead the team in development of SmartKPIs, a goal tracking application for the employees of an organisation using React.js, Drupal 8, PHP, Sass, Webpack. The application allows employees to add and track their goals for their KPIs and review the team members goals for better KPI discussions along with a notification system and customised dashboard.',
+          stack: 'React.js, Drupal 8, PHP, Twig, MySQL, Webpack, Sass'
+        },
+        {
+          title: 'MyLook',
+          url: 'https://mylook.acuvue.com/',
+          description: 'Developed MyLook website that enables Beauty experience as an omni-channel guided commerce capability using augmented reality, making it easier for the users to try and buy contact lenses.',
+          stack: 'Node.js, React.js, Redux, Sass, Webpack, AWS'
+        },
+        {
+          title: 'Openbank Dev Portal',
+          description: 'Integrated Openbank apis from Apigee platform into a website to be used as a playground for apis working.',
+          stack: 'Drupal 8, PHP, React.js, Javascript, MySQL Sass, Bootstrap, Webpack'
+        },
+        {
+          title: 'iLoveLibraries',
+          url: 'http://www.ilovelibraries.org',
+          description: 'Added new features and styles to the website.',
+          stack: 'Drupal 7, PHP, Javascript, MySQL, Sass, Gulp'
+        },
+        {
+          title: 'UNCCD',
+          url: 'https://www.unccd.int',
+          description: 'Developed Q&A feature for droughts related information for the official portal of United Nations Convention to Combat Desertification. Designed the front-end of the website.',
+          stack: 'Drupal 8, PHP, Javascript, Twig, MySQL, LESS, Grunt'
+        },
+        {
+          title: 'InforMEA',
+          url: 'https://www.informea.org',
+          description: 'Developed Glossary and autocomplete search feature for categories on the portal and setup analytics on the pages of the website. Designed the front-end of the website.',
+          stack: 'Drupal 7, PHP, Javascript, MySQL, LESS, Grunt'
+        },
+        {
+          title: 'Adidas CTC',
+          description: 'Developed features for the B2B E-commerce website used by Adidas stores to purchase products. Designed the front-end of the website.',
+          stack: 'Drupal 8, PHP, Javascript, Twig, MySQL, Sass, Gulp'
+        },
+        {
+          title: 'Developer Portal',
+          description: 'Developed a portal to be used by developers to get the information of apis provided by Globe Labs.',
+          stack: 'Drupal 7, PHP, Javascript, MySQL, Sass, Gulp'
+        },
+        {
+          title: 'St. Jerome’s University',
+          url: 'https://www.sju.ca/',
+          description: 'Developed website for St. Jerome’s University providing access to its academic programmes. Designed the front-end of the website.',
+          stack: 'Drupal 7, PHP, Javascript, MySQL, Sass, Grunt'
+        }
+      ]
+    },
+    {
+      position: 'Senior Web Developer',
+      organisation: 'Rawpixel Ltd.',
+      org_desc: 'World leading creator of stock images',
+      from: 'June 2018',
+      to: 'August 2018',
+      projects: [
+        {
+          title: 'Rawpixel',
+          url: 'https://www.rawpixel.com',
+          description: 'Maintained and supported live website. Developed and designed new plugins with respect to requirement.',
+          stack: 'Drupal 7, PHP, Node.js, Javascript, PostCSS, MySQL, Webpack'
+        }
+      ]
+    },
+    {
+      position: 'Senior Frontend Developer',
+      organisation: 'Srijan Technologies',
+      org_desc: 'Leading Drupal agency in India',
+      from: 'February 2018',
+      to: 'June 2018',
+      projects: [
+        {
+          title: 'Learning Management System',
+          description: 'Developed a tool to track learning of the employees.',
+          stack: 'Drupal 8, PHP, React.js, Redux, MySQL, Sass, Webpack'
+        },
+        {
+          title: 'NALX',
+          description: 'Developed REST APIs as backend service for a B2B frontend portal.',
+          stack: 'Drupal 8, PHP, MySQL'
+        }
+      ]
+    },
+    {
+      position: 'Senior Software Engineer',
+      organisation: 'EX2 Solutions India',
+      from: 'December 2016',
+      to: 'January 2018',
+      projects: [
+        {
+          title: 'WebArch',
+          description: 'Lead the design architecture and front-end chapter of the product.',
+          stack: 'Drupal 8, PHP, Javascript, React.js, MySQL, Sass, Gulp'
+        },
+        {
+          title: 'Herdactiy',
+          url: 'https://herdacity.org',
+          description: 'Developed custom features and designed the frontend of the website.',
+          stack: 'Drupal 8, PHP, Javascript, Angular 4, MySQL, Sass, Bootstrap, Gulp'
+        },
+        {
+          title: 'TexDEx',
+          url: 'http://www.texasdefenseexchange.com',
+          description: 'Developed website showcasing information of contractors and their work in the defence industry.',
+          stack: 'Drupal 8, Javascript, Angular 5, Twig, Sass'
+        }
+      ]
+    },
+    {
+      position: 'Frontend Developer',
+      organisation: 'Axelerant Technologies',
+      org_desc: 'Leading Drupal agency in India',
+      from: 'July 2015',
+      to: 'November 2016',
+      projects: [
+        {
+          title: 'Mayfair',
+          url: 'https://www.mayfairhouse.com',
+          description: 'Designed and developed front end theme for the e-commerce store.',
+          stack: 'Drupal 7, PHP, MySQL, jQuery, HTML, Sass, Gulp'
+        },
+        {
+          title: 'Scouts',
+          url: 'https://www.scout.org',
+          description: 'Designed and developed front end theme for the Drupal system used for the website.',
+          stack: 'Drupal 7, PHP, jQuery, MySQL, HTML, Sass, Bootstrap, Gulp'
+        }
+      ]
+    },
+    {
+      position: 'Software Developer',
+      organisation: 'Wipro Technologies',
+      org_desc: 'Leading software house in India',
+      from: 'January 2012',
+      to: 'July 2015',
+      projects: [
+        {
+          title: 'D2C, Sony Music',
+          description: 'Designed and developed websites for the artists with Sony Music group.',
+          stack: 'Drupal 6/7, Wordpress, PHP, jQuery, MySQL, HTML, CSS, Sass'
+        },
+        {
+          title: 'WMG, Acquia',
+          description: 'Migrated websites of Warner Music group from Acquia Dev cloud to Acquia Sites Factory.',
+          stack: 'Drupal 7, jQuery, HTML, CSS'
+        },
+        {
+          title: 'FOX',
+          description: '​Designed the clean front-end structure of the website for Fox Networks Group.',
+          stack: 'Drupal 7, Javascript, PHP, MySQL, Sass'
+        }
+      ]
+    }
+  ],
+  education: [
+    {
+      school: 'University of Jammu',
+      course: 'Bachelor of Engineering',
+      major: 'Computer Engineering',
+      from: 'August 2007',
+      to: 'July 2011'
+    }
+  ],
+  skills: {
+    programming: [
+      'Javascript',
+      'React.js',
+      'Angular',
+      'Web Components',
+      'lit-html',
+      'LitElement',
+      'Typescript',
+      'Node.js',
+      'PHP',
+      'jQuery',
+      'ES6',
+      'Redux',
+      'GraphQL'
+    ],
+    cms: [
+      'Drupal',
+      'WordPress'
+    ],
+    frontend: [
+      'HTML5',
+      'CSS3',
+      'Sass/LESS',
+      'PostCSS'
+    ],
+    database: [
+      'MySQL',
+      'MongoDB',
+      'PostgreSQL'
+    ],
+    tools: [
+      'Gulp',
+      'Grunt',
+      'Webpack'
+    ],
+    other: [
+      'Agile',
+      'AWS',
+      'Azure',
+      'Git',
+      'Responsive web design',
+      'PSD to HTML conversion'
+    ]
+  },
+  interests: [
+    'Apart from being a web developer, I enjoy most of my time being outdoors. I find road trips, motorbikes and the countryside to be my calling.',
+    'When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.'
+  ]
+};
